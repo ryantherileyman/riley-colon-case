78bb114ca20fda5f8843ced866b8e9418c8b572a
Ryan Robinson <ryantherileyman@outlook.com>
2021-01-23 08:42:30 -0600

    Add message to indicate all tests passed

--- ColonCase/ColonCaseGame/src/test/validation/r3-validation-test-main.cpp
+++ ColonCase/ColonCaseGame/src/test/validation/r3-validation-test-main.cpp
@@ -1,11 +1,11 @@
 
 #include <assert.h>
+#include <stdio.h>
 #include "r3-validation-test.hpp"
 
 using namespace r3::validation;
 
 int main() {
-
 	assert(IntRangeTests::testIsInRange_MinOnly(0, 1, true));
 	assert(IntRangeTests::testIsInRange_MinOnly(0, 0, true));
 	assert(IntRangeTests::testIsInRange_MinOnly(0, -1, false));
@@ -19,5 +19,6 @@ int main() {
 	assert(IntRangeTests::testIsInRange_MinAndMax(3, 5, 5, true));
 	assert(IntRangeTests::testIsInRange_MinAndMax(3, 5, 6, false));
 
+	printf("All tests passed!\n");
 	return 0;
 }
