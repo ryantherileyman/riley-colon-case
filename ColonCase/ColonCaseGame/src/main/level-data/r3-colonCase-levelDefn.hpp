7b6a5b8460708045985396bf8965a87f94dfd455
Ryan Robinson <ryantherileyman@outlook.com>
2021-01-24 14:24:45 -0600

    TCC-15 Initial map and tile structures for Colon Case gameplay

---
+++ ColonCase/ColonCaseGame/src/main/level-data/r3-colonCase-levelDefn.hpp
@@ -0,0 +1,46 @@
+
+#include <unordered_map>
+#include <SFML/Graphics.hpp>
+#pragma once
+
+namespace r3 {
+
+	namespace colonCase {
+
+		typedef struct ColonCase_GameTileImageDefn {
+			int tileId = 0;
+			std::string filename;
+			sf::Vector2i imageSize;
+			sf::IntRect textureRect;
+		} GameTileImageDefn;
+
+		typedef enum class ColonCase_GameMapLayerType {
+			UNKNOWN,
+			TILE,
+			SPRITE,
+		} GameMapLayerType;
+		
+		typedef struct ColonCase_GameMapSpriteDefn {
+			int tileId = 0;
+			sf::Vector2f position;
+			sf::Vector2f size;
+		} GameMapSpriteDefn;
+
+		typedef struct ColonCase_GameMapLayerDefn {
+			GameMapLayerType layerType = GameMapLayerType::UNKNOWN;
+			bool renderFlag = false;
+			bool collisionFlag = false;
+			std::vector<int> tileIdList;
+			std::vector<GameMapSpriteDefn> spriteDefnList;
+		} GameMapLayerDefn;
+
+		typedef struct ColonCase_GameMapDefn {
+			sf::Vector2i size;
+			sf::Color backgroundColor;
+			std::unordered_map<int, GameTileImageDefn> tileImageDefnMap;
+			std::vector<GameMapLayerDefn> layerDefnList;
+		} GameMapDefn;
+
+	}
+
+}
